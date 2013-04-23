9d63bc8c4ec01a623062f2c3401f955291b10ea6
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-04-24 01:39:53 +0800

    Initial Commit

---
+++ MapTestTests/MapTestTests.m
@@ -0,0 +1,32 @@
+//
+//  MapTestTests.m
+//  MapTestTests
+//
+//  Created by Peter Pan on 4/24/13.
+//  Copyright (c) 2013 Peter Pan. All rights reserved.
+//
+
+#import "MapTestTests.h"
+
+@implementation MapTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapTestTests");
+}
+
+@end
