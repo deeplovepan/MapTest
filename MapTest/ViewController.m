67b3d17228417891c701f276d8d9b73d7a6b53fc
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-04-25 01:07:54 +0800

    addAnnotation

--- MapTest/ViewController.m
+++ MapTest/ViewController.m
@@ -8,9 +8,12 @@
 
 #import "ViewController.h"
 #import <MapKit/MapKit.h>
+#import "MyAnnotation.h"
 
 @interface ViewController () <MKMapViewDelegate>
-
+{
+    CLLocationCoordinate2D coordinate101;
+}
 @property (weak, nonatomic) IBOutlet MKMapView *testMapView;
 
 
@@ -23,7 +26,12 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-   }
+    coordinate101 = CLLocationCoordinate2DMake(25.033408, 121.564099);
+    MyAnnotation *annotation = [[MyAnnotation alloc] initWithCoordinate:coordinate101 title:@"台北101" subtitle:@"信義誠品隔壁"];
+    [self.testMapView addAnnotation:annotation];
+
+    
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -35,7 +43,10 @@
 
 -(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
-    self.testMapView.region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 100, 100);
+    MKCoordinateRegion region;
+    region.center = userLocation.coordinate;
+    region.span = MKCoordinateSpanMake(ABS(coordinate101.latitude-userLocation.coordinate.latitude)*2, ABS(coordinate101.longitude-userLocation.coordinate.longitude)*2);
+    self.testMapView.region = region;
 
 }
 
